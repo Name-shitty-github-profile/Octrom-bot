03739148c46413f1551457596ea8ffdf7fbb0075
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 02:32:25 -0500

    NO MORE SETTINGS

--- src/core/bot.js
+++ src/core/bot.js
@@ -1,14 +1,44 @@
 const settings = require('./settings');
 const eventManager = require('./eventManager');
 const slashFetcher = require('./slashFetcher');
-const { Client } = require('discord.js');
-const { token } = require(`../../privates_files/config`);
 
+const { 
+    Client,
+    GatewayIntentBits, 
+    Partials
+} = require('discord.js');
+
+const { token } = require(`../../privates_files/config`);
 
 const client = new Client(
     {
-        intents: settings.Intents,
-        partials: settings.Partials,
+        intents: [
+            GatewayIntentBits.AutoModerationConfiguration, 
+            GatewayIntentBits.AutoModerationExecution, 
+            GatewayIntentBits.DirectMessageReactions, 
+            GatewayIntentBits.GuildWebhooks,
+            GatewayIntentBits.DirectMessages, 
+            GatewayIntentBits.GuildBans, 
+            GatewayIntentBits.GuildEmojisAndStickers, 
+            GatewayIntentBits.GuildIntegrations, 
+            GatewayIntentBits.DirectMessageTyping,
+            GatewayIntentBits.GuildInvites, 
+            GatewayIntentBits.GuildMembers, 
+            GatewayIntentBits.GuildMessageReactions, 
+            GatewayIntentBits.GuildMessageTyping, 
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMessages, 
+            GatewayIntentBits.GuildModeration, 
+            GatewayIntentBits.GuildPresences, 
+            GatewayIntentBits.GuildScheduledEvents, 
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildVoiceStates
+        ],
+        partials: [
+            Partials.Channel,
+            Partials.Reaction,
+            Partials.Message
+        ],
         ws: {
             properties: {
                 browser: 'Discord iOS'
