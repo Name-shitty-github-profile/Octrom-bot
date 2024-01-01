7d19c901fbf54125467ee733c924aab51075d9d6
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 02:19:06 -0500

    Update eventManager.js

--- src/core/eventManager.js
+++ src/core/eventManager.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 module.exports = (client) => {
     const eventFiles = fs.readdirSync(`${__dirname}/../Events`).filter(file => file.endsWith('.js'));
-    let i = 0;
     for (const file of eventFiles) {
         const event = require(`${__dirname}/../Events/${file}`);
         const eventName = file.split('.')[0];
         client.on(eventName, event.bind(null, client));
-        i++;
     }
-    console.log(`Event : ${i} has been loaded!`);
+    console.log(`Event : ${eventFiles.length} has been loaded!`);
 };
