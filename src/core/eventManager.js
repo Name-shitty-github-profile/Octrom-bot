1f41d16c6843047149ec8246966624619c1be439
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 02:24:01 -0500

    Update eventManager.js
    DUMB THING BUT USING LESS RAM LETS FUCKING GOOOO

--- src/core/eventManager.js
+++ src/core/eventManager.js
@@ -5,7 +5,7 @@ module.exports = (client) => {
     const eventFiles = fs.readdirSync(`${__dirname}/../Events`).filter(file => file.endsWith('.js'));
     for (const file of eventFiles) {
         const event = require(`${__dirname}/../Events/${file}`);
-        const eventName = file.split('.')[0];
+        const eventName = file.replace(".js", "");
         client.on(eventName, event.bind(null, client));
     }
     console.log(`Event : ${eventFiles.length} has been loaded!`);
