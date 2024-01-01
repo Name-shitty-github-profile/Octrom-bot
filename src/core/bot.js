b30eabc9c19932eadcfe6f250f04c58d3b7dcc5b
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 02:17:03 -0500

    Update bot.js

--- src/core/bot.js
+++ src/core/bot.js
@@ -1,6 +1,28 @@
+const settings = require('./settings');
+const eventManager = require('./eventManager');
+const slashFetcher = require('./slashFetcher');
 const { Client } = require('discord.js');
 const { token } = require(`../../privates_files/config`);
-const settings = require('./settings');
-const client = new Client({intents: settings.Intents, partials: settings.Partials, ws: {properties: {browser: 'Discord iOS'}}});
-require("../core")(client);
+
+
+const client = new Client(
+    {
+        intents: settings.Intents,
+        partials: settings.Partials,
+        ws: {properties: {
+            browser: 'Discord iOS'
+        }
+    }}
+);
+
+client.commands = new Map();
+
+client.interactions = new Map();
+
+client.register_arr = [];
+
+eventManager(client),
+
+slashFetcher(client);
+
 client.login(token);
