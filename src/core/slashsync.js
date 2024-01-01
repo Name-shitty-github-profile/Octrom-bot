1fb4a2de8a0827db46855fd1490eb3c96f288d8e
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 03:07:37 -0500

    Update slashsync.js

--- src/core/slashsync.js
+++ src/core/slashsync.js
@@ -2,8 +2,8 @@ module.exports = async (client, commands) => {
     const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
     await ready;
     console.log(`Synchronizing commands...`);
-    for (let Command of commands) {
-        await client.application.commands.create(Command);
+    for (let command of commands) {
+        await client.application.commands.create(command);
     }
     console.log(`Created ${commands.length} commands!`);
     console.log(`Commands synchronized!`);
