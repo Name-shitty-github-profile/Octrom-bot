5cd32737be998a97bb88b2ff447a534be7e27ea2
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 02:59:46 -0500

    Update slashFetcher.js

--- src/core/slashFetcher.js
+++ src/core/slashFetcher.js
@@ -3,27 +3,23 @@ const path = require('path');
 
 function getSlashs(folderPath) {
     let files = [];
-    function traverseFolder(currentPath) {
-        const items = fs.readdirSync(currentPath);
-        items.forEach((item) => {
-            const itemPath = path.join(currentPath, item);
-            const stat = fs.statSync(itemPath);
-            if (stat.isDirectory()) {
-                traverseFolder(itemPath);
-            } else {
-                files.push(itemPath.replace(/\.js$/, ''));
-            }
-        });
+    const items = fs.readdirSync(folderPath);
+    for (const item of items) {
+        const itemPath = path.join(folderPath, item);
+        if (fs.statSync(itemPath).isDirectory()) {
+            files.push(...getSlashs(itemPath));
+        } else {
+            files.push(itemPath.replace(/\.js$/, ''));
+        }
     }
-    traverseFolder(folderPath);
     return files;
 }
 
 module.exports = async (client) => {
     const slash_commands = getSlashs(`${process.cwd()}/src/Slash`);
-    slash_commands.forEach(command => {
+    for (const command of slash_commands) {
         const props = require(command);
         client.interactions.set(props.name, { ...props });
         client.register_arr.push(props);
-    });
+    }
 };
