934b4ebe43334c266832d177a6653b4cdf8c9a35
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-01 02:52:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { token } = require('./privates_files/config');
 
 const manager = new ShardingManager("./src/core/bot.js", { totalShards: "auto", token: token});
 
-manager.on("shardCreate", (shard) => console.log(`Hello! The bot started! (Shard[${shard.id}])`));
+manager.on("shardCreate", (shard) => {
+    console.log(`Hello! The bot started! (Shard[${shard.id}])`);
+});
 
 manager.spawn();
