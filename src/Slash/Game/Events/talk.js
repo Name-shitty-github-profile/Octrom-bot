825816998d7a54d5b3be4f272d0356e84292c220
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-02 18:47:40 -0500

    Update talk.js

--- src/Slash/Game/Events/talk.js
+++ src/Slash/Game/Events/talk.js
@@ -3,6 +3,6 @@ const Talks = require(`${process.cwd()}/utils/GetEventData`)("talk");
 module.exports = {
     name: "talk",
     func: async function(client, interaction) {
-        
+        await Talks[Math.floor(Math.random() *Talks.length)](client, interaction);
     }
 }
