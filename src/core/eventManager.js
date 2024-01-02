6507ec14a0b3ba8c0add09d06365676ee8b4047f
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2024-01-02 01:31:32 -0500

    Update eventManager.js

--- src/core/eventManager.js
+++ src/core/eventManager.js
@@ -4,9 +4,12 @@ const path = require('path');
 module.exports = (client) => {
     const eventFiles = fs.readdirSync(`${__dirname}/../Events`).filter(file => file.endsWith('.js'));
     for (const file of eventFiles) {
-        const event = require(`${__dirname}/../Events/${file}`);
-        const eventName = file.replace(".js", "");
-        client.on(eventName, event.bind(null, client));
+        /*
+        VARIABLES FOR FUNCTION (OPTIMIZED RAM USAGE) :
+        event = require(`${__dirname}/../Events/${file}`);
+        eventName = file.replace(".js", "");
+        */
+        client.on(file.replace(".js", ""), require(`${__dirname}/../Events/${file}`).bind(null, client));
     }
     console.log(`${eventFiles.length} events has been loaded!`);
 };
